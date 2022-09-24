cc50a5a4b28e013df167976fd2f30c5f1e06a0a6
mohdbilal <mohdbilal3386@gmail.com>
2022-09-25 01:07:23 +0530

    Making page Protected

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,21 +1,20 @@
-import React, { Fragment, useContext} from "react";
+import React, { Fragment, } from "react";
 import {NavLink} from 'react-router-dom'
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 import HeaderBottom from "./HeaderBottom";
-import AuthContext from "../../store/auth-context";
+
 
 const Header = props => {
-  const authCtx = useContext(AuthContext)
- 
+
   return (
     <Fragment>
       <header className={classes.header}>
-       <NavLink activeClassName={classes.active} to="/">HOME</NavLink>
-       {authCtx.isLoggedIn && <NavLink activeClassName={classes.active} to="/store">STORE</NavLink>}
-        {authCtx.isLoggedIn && <NavLink activeClassName={classes.active} to="/about">ABOUT</NavLink>}
-        {!authCtx.isLoggedIn && <NavLink activeClassName={classes.active} to="/login">LOGIN</NavLink>}
-        {authCtx.isLoggedIn && <NavLink activeClassName={classes.active} to="/contact-us">CONTACT-US</NavLink>}
+       <NavLink exact activeClassName={classes.active} to="/">HOME</NavLink>
+         <NavLink activeClassName={classes.active} to="/store">STORE</NavLink>
+        <NavLink activeClassName={classes.active} to="/about">ABOUT</NavLink>
+         <NavLink activeClassName={classes.active} to="/login">LOGIN</NavLink>
+        <NavLink activeClassName={classes.active} to="/contact-us">CONTACT-US</NavLink>
         <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <HeaderBottom/>
